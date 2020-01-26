b100a5db14eaa81aa7408883d6a29501427d7922
Xavier Puspus <xpuspus@gmail.com>
2020-01-26 11:27:56 +0800

    Rush commit

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# dow-jones-sentiment-predictor-webapp
-Using news article data, predict stock trend (up or down).
+# Down Jones Trend Predictor Web Application  
+Author: Xavier M. Puspus  
+
+
+### Description
+I used a sample dataset found [here](https://www.kaggle.com/aaron7sun/stocknews#Combined_News_DJIA.csv). I built a model that predicts stock trends based on news.
+  
+### Model
+
+I used simple ensemble methods using news text data as features to predict 1 for upward trend, and 0 for downward.
+
+### Deployment Through Web Application
+
+I used the most recently released API of [Streamlit](https://streamlit.io) to deploy the ml model and locally serve the web app.
+
+### Running the App
+
+In order to run the app, you must have the basic data science packages available on your machine, (`pandas`, `numpy`, `matplotlib`, `sklearn` and install streamlit using:
+
+```console
+foo@bar:~$ pip install streamlit
+```
+Afterwards, `cd` into the directory of `app.py` and run this on the terminal:
+
+```console
+foo@bar:~$ streamlit run app.py
+```
+
+### Display
+
+The web app should look something like this:
+
+![Sample image of the Down Jones Index predictor web application.](dji_webapp_image.png)
---
+++ app.py
@@ -0,0 +1,65 @@
+import pandas as pd
+import joblib
+
+import nltk
+from nltk.corpus import stopwords 
+from nltk.stem.wordnet import WordNetLemmatizer
+import string
+
+import gensim
+from gensim import corpora
+from gensim.models import CoherenceModel
+
+import streamlit as st
+
+nltk.download('stopwords')
+nltk.download('wordnet')
+
+# Header
+### Set Title
+st.title("Dow Jones Trend Predictor")
+st.write("""From the input news, predict upward or downward trend.""")
+
+# Text input
+text = st.text_area("Input news", 'Input text here.')
+
+
+# Function to Clean text
+stop = set(stopwords.words('english'))
+exclude = set(string.punctuation) 
+lemma = WordNetLemmatizer()
+def clean(doc):
+    stop_free = " ".join([i for i in doc.lower().split() if i not in stop])
+    punc_free = ''.join(ch for ch in stop_free if ch not in exclude)
+    normalized = " ".join(lemma.lemmatize(word) for word in punc_free.split())
+    return normalized
+
+# Actually clean text
+text_process = clean(text).split()
+
+# Load dictionary 
+loaded_dictionary = corpora.Dictionary.load('models/dictionary.sav') 
+
+# Convert list of words to document term array
+doc_term = loaded_dictionary.doc2bow(text_process)
+
+# Load LDA model
+lda_load_model = gensim.models.ldamodel.LdaModel.load("models/lda_model.sav")
+
+# Get document topic probabilities
+lists = lda_load_model.get_document_topics(doc_term, minimum_probability=0.0)
+
+# Convert to array/list
+document_topic =  [i[1] for i in lists]
+
+# Load regression model
+regression_model = joblib.load('models/lr_model.sav')
+
+# predict on output document topic
+prediction = regression_model.predict([document_topic])[0]
+
+# Predicted rating
+if prediction > 0:
+    st.write(f'Predicted Dow Jones Index trend is upwards.')
+else:
+    st.write(f'Predicted Dow Jones Index trend is downwards.')
